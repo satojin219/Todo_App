48f2d1b9882ad5fbd802a2670ab1c40243ff84ed
satojin <seiron1212@outlook.jp>
2022-05-29 21:07:30 +0900

    get todos

--- main.go
+++ main.go
@@ -9,13 +9,21 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	u.CreateUser()
-	user, _ := models.GetUser(2)
-	user.CreateTodo("First Todo")
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// u.CreateUser()
+	// user, _ := models.GetUser(2)
+	// user.CreateTodo("First Todo")
+	// t,_ := models.GetTodo(1)
+	// fmt.Println(t)
+	user,_ := models.GetUser(2)
+	user.CreateTodo("Second Todo")
+	todos,_ := models.GetTodos()
+	for _,v:= range todos{
+		fmt.Println(v)
+	}
 
 	// u.Name = "Test2"
 	// u.Email = "test2@example.com"
