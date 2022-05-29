361677a48172fc359fca6a76f06775849ced0f80
satojin <seiron1212@outlook.jp>
2022-05-29 20:03:32 +0900

    get User

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
+	// u.CreateUser()
+	u,_ :=models.GetUser(1)
 	fmt.Println(u)
-	u.CreateUser()
 }
