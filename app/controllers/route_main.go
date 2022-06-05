ef6c8b44cabbfb2af6596668f90fd9801008e126
satojin <seiron1212@outlook.jp>
2022-06-05 13:31:20 +0900

    sign up

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,15 +1,9 @@
 package controllers
 
 import (
-	"html/template"
-	"log"
 	"net/http"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("app/views/template/top.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	t.Execute(w, nil)
+	generateHTML(w,"Hello","layout","public_navbar","top")
 }
